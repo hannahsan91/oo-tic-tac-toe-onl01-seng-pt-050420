e5c03cf4c082e3c9910798ed5007ac25dfbc6b54
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-27 02:41:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,12 +7,18 @@ class TicTacToe
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
-  [6,7,8]
+  [6,7,8],
+  [0,4,8],
+  [1,4,7],
+  [2,4,6],
+  [0,3,6],
+  [2,5,8]
 ]
 
 def display_board
   puts @board
+end
 
-
+def input_to_index
 
 end
