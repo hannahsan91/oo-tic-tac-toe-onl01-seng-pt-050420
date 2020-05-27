9fbd239db0a89579e751e3c3f33d6fead9055a27
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-27 02:48:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,15 +15,19 @@ WIN_COMBINATIONS = [
   [2,5,8]
 ]
 
-def display_board
+  def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
-end
+  end
 
-def input_to_index
+  def input_to_index(user_selection)
+  user_input_to_integer = user_selection.to_i
+  user_input_to_integer-1
+  end
+
 
 
 end
